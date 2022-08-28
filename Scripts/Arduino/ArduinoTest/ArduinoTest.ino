4b4079bc6d67faf519ae4311d4872f932be58450
CimttGit <111210419+CimttGit@users.noreply.github.com>
2022-08-27 20:42:08 -0400

    Update ArduinoTest.ino

--- Scripts/Arduino/ArduinoTest/ArduinoTest.ino
+++ Scripts/Arduino/ArduinoTest/ArduinoTest.ino
@@ -1,17 +1,37 @@
 #define PIN_BUZZER 11 // Definir el pin de salida del BUZZER
+int incomingByte; // Para la entrada de datos en serie
+int pinVibrator = 3;  // Para selecionar pin de salida del vibrador
+
 void setup() {
     // Inicializar el buzzer
     pinMode(PIN_BUZZER, OUTPUT);
     digitalWrite(PIN_BUZZER, LOW);
+    Serial.begin(9600); // opens serial port, sets data rate to 9600 bps
 }
 
 void loop() {
-    // Hacer sonar el buzzer con una frecuencia de 440Hz (Nota A4) y duración de 1 segundo (1000 milisegundos)
-    tone(PIN_BUZZER, 440, 1000);
-    // Esperar media segundo
-    delay(500);     
-    // Hacer sonar el buzzer con una frecuencia de 880Hz (Nota B4) y duración de 1 segundo
-    tone(PIN_BUZZER, 880, 1000);
-    // detener loop de sonido con un buble infinito vacío
-    while(1){ }
+  
+  // Hacer sonar el buzzer con una frecuencia de 440Hz (Nota A4) y duración de 1 segundo (1000 milisegundos)
+  tone(PIN_BUZZER, 440, 1000);
+  // Esperar media segundo
+  delay(500);     
+  // Hacer sonar el buzzer con una frecuencia de 880Hz (Nota B4) y duración de 1 segundo
+  tone(PIN_BUZZER, 880, 1000);
+  
+
+  while(1){
+    if (Serial.available() > 0) { // send data only when you receive data:
+      incomingByte = Serial.read()-48;
+      if (incomingByte == 1)
+        pinVibrator = 3;
+      if (incomingByte == 2)
+        pinVibrator = 5;
+      if (incomingByte == 3)
+        pinVibrator = 6;
+      if (incomingByte == 4)
+        pinVibrator = 11;
+      Serial.println(incomingByte);
+      tone(pinVibrator, 440, 100);
+    }
+  }
 }
