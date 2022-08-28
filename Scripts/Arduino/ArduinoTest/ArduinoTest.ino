fcfc109eb1c641ea47f6bdb8e6b770a9a7816eb0
CimttGit <cursos.cimtt@gmail.com>
2022-08-27 21:14:00 -0400

    upload changes

--- Scripts/Arduino/ArduinoTest/ArduinoTest.ino
+++ Scripts/Arduino/ArduinoTest/ArduinoTest.ino
@@ -11,27 +11,27 @@ void setup() {
 
 void loop() {
   
-  // Hacer sonar el buzzer con una frecuencia de 440Hz (Nota A4) y duración de 1 segundo (1000 milisegundos)
-  tone(PIN_BUZZER, 440, 1000);
-  // Esperar media segundo
-  delay(500);     
-  // Hacer sonar el buzzer con una frecuencia de 880Hz (Nota B4) y duración de 1 segundo
-  tone(PIN_BUZZER, 880, 1000);
-  
-
-  while(1){
-    if (Serial.available() > 0) { // send data only when you receive data:
-      incomingByte = Serial.read()-48;
-      if (incomingByte == 1)
-        pinVibrator = 3;
-      if (incomingByte == 2)
-        pinVibrator = 5;
-      if (incomingByte == 3)
-        pinVibrator = 6;
-      if (incomingByte == 4)
-        pinVibrator = 11;
-      Serial.println(incomingByte);
-      tone(pinVibrator, 440, 100);
+    // Hacer sonar el buzzer con una frecuencia de 440Hz (Nota A4) y duración de 1 segundo (1000 milisegundos)
+    tone(PIN_BUZZER, 440, 1000);
+    // Esperar media segundo
+    delay(500);     
+    // Hacer sonar el buzzer con una frecuencia de 880Hz (Nota B4) y duración de 1 segundo
+    tone(PIN_BUZZER, 880, 1000);
+    
+    // Comunicación serial
+    while(1){
+        if (Serial.available() > 0) { // send data only when you receive data:
+        incomingByte = Serial.read()-48;
+        if (incomingByte == 1)
+            pinVibrator = 3;
+        if (incomingByte == 2)
+            pinVibrator = 5;
+        if (incomingByte == 3)
+            pinVibrator = 6;
+        if (incomingByte == 4)
+            pinVibrator = 11;
+        Serial.println(incomingByte);
+        tone(pinVibrator, 440, 100);
+        }
     }
-  }
 }
