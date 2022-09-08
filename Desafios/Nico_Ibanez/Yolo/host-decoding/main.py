5f77a95f23be1c7cf9949fb4b2b79067163ef430
CimttGit <cursos.cimtt@gmail.com>
2022-09-08 13:11:25 -0400

    .

--- Desafios/Nico_Ibanez/Yolo/host-decoding/main.py
+++ Desafios/Nico_Ibanez/Yolo/host-decoding/main.py
@@ -22,13 +22,13 @@ You can clone the YoloV5_training.ipynb notebook and try training the model your
 
 '''
 labelMap = [
-    "person",         "bicycle",    "car",           "motorbike",     "aeroplane",   "bus",           "train",
-    "truck",          "boat",       "traffic light", "fire hydrant",  "stop sign",   "parking meter", "bench",
+    "Persona",        "Bicicleta",  "Auto",          "Moto",          "aeroplane",   "Bus",           "Tren",
+    "truck",          "boat",       "Semaforo",      "fire hydrant",  "stop sign",   "parking meter", "bench",
     "bird",           "cat",        "dog",           "horse",         "sheep",       "cow",           "elephant",
     "bear",           "zebra",      "giraffe",       "backpack",      "umbrella",    "handbag",       "tie",
     "suitcase",       "frisbee",    "skis",          "snowboard",     "sports ball", "kite",          "baseball bat",
     "baseball glove", "skateboard", "surfboard",     "tennis racket", "bottle",      "wine glass",    "cup",
-    "fork",           "knife",      "spoon",         "bowl",          "banana",      "apple",         "sandwich",
+    "fork",           "knife",      "spoon",         "bowl",          "banana",      "Manzana",         "sandwich",
     "orange",         "broccoli",   "carrot",        "hot dog",       "pizza",       "donut",         "cake",
     "chair",          "sofa",       "pottedplant",   "bed",           "diningtable", "toilet",        "tvmonitor",
     "laptop",         "mouse",      "remote",        "keyboard",      "cell phone",  "microwave",     "oven",
