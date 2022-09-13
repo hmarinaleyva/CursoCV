3366e3fa11842d080ab3dbc67327012a417387ed
CimttGit <cursos.cimtt@gmail.com>
2022-09-13 18:33:33 -0300

    jk

--- Projects/main.py
+++ Projects/main.py
@@ -1,4 +1,4 @@
-from Projects.utilities import *
+from utilities import *
 from depthai_sdk import Previews, FPSHandler
 from depthai_sdk.managers import PipelineManager, PreviewManager, BlobManager, NNetManager
 import numpy as np
@@ -13,6 +13,8 @@ os.chdir(MainDir)
 MODEL_PATH = os.path.join(MainDir, '../Models/MetroModel_YOLOv5s', "Metro_openvino_2021.4_6shave.blob")
 CONFIG_PATH = os.path.join(MainDir, '../Models/MetroModel_YOLOv5s', "Metro.json")
 
+
+############################################################################################################
 # initialize blob manager with path to the blob
 bm = BlobManager(blobPath=MODEL_PATH)
 
@@ -30,6 +32,10 @@ pv = PreviewManager(display=[Previews.color.name], fpsHandler=fpsHandler)
 nn = nm.createNN(pipeline=pm.pipeline, nodes=pm.nodes, source=Previews.color.name,
                  blobPath=bm.getBlob(shaves=6, openvinoVersion=pm.pipeline.getOpenVINOVersion(), zooType="depthai"))
 pm.addNn(nn)
+############################################################################################################
+
+
+############################################################################################################
 # Create pipeline
 pipeline = dai.Pipeline()
 
@@ -59,57 +65,60 @@ stereo.depth.link(xoutDepth.input)
 xoutDepth = pipeline.create(dai.node.XLinkOut)
 xoutDepth.setStreamName("disp")
 stereo.disparity.link(xoutDepth.input)
+############################################################################################################
 
 # Connect to device and start pipeline
-with dai.Device(pipeline) as device:
-    # Output queue will be used to get the depth frames from the outputs defined above
-    depthQueue = device.getOutputQueue(name="depth")
-    dispQ = device.getOutputQueue(name="disp")
 
-    text = TextHelper()
-    hostSpatials = HostSpatialsCalc(device)
-    y = 200
-    x = 300
-    step = 3
-    delta = 5
-    hostSpatials.setDeltaRoi(delta)
+device = dai.Device(pipeline)
 
-    print("Use WASD keys to move ROI.\nUse 'r' and 'f' to change ROI size.")
+# Output queue will be used to get the depth frames from the outputs defined above
+depthQueue = device.getOutputQueue(name="depth")
+dispQ = device.getOutputQueue(name="disp")
 
-    while True:
-        depthFrame = depthQueue.get().getFrame()
-        # Calculate spatial coordiantes from depth frame
-        spatials, centroid = hostSpatials.calc_spatials(depthFrame, (x,y)) # centroid == x/y in our case
+text = TextHelper()
+hostSpatials = HostSpatialsCalc(device)
+y = 200
+x = 300
+step = 3
+delta = 5
+hostSpatials.setDeltaRoi(delta)
 
-        # Get disparity frame for nicer depth visualization
-        disp = dispQ.get().getFrame()
-        disp = (disp * (255 / stereo.initialConfig.getMaxDisparity())).astype(np.uint8)
-        disp = cv2.applyColorMap(disp, cv2.COLORMAP_JET)
+print("Use WASD keys to move ROI.\nUse 'r' and 'f' to change ROI size.")
 
-        text.rectangle(disp, (x-delta, y-delta), (x+delta, y+delta))
-        text.putText(disp, "X: " + ("{:.1f}m".format(spatials['x']/1000) if not math.isnan(spatials['x']) else "--"), (x + 10, y + 20))
-        text.putText(disp, "Y: " + ("{:.1f}m".format(spatials['y']/1000) if not math.isnan(spatials['y']) else "--"), (x + 10, y + 35))
-        text.putText(disp, "Z: " + ("{:.1f}m".format(spatials['z']/1000) if not math.isnan(spatials['z']) else "--"), (x + 10, y + 50))
+while True:
+    depthFrame = depthQueue.get().getFrame()
+    # Calculate spatial coordiantes from depth frame
+    spatials, centroid = hostSpatials.calc_spatials(depthFrame, (x,y)) # centroid == x/y in our case
 
-        # Show the frame
-        cv2.imshow("depth", disp)
+    # Get disparity frame for nicer depth visualization
+    disp = dispQ.get().getFrame()
+    disp = (disp * (255 / stereo.initialConfig.getMaxDisparity())).astype(np.uint8)
+    disp = cv2.applyColorMap(disp, cv2.COLORMAP_JET)
 
-        key = cv2.waitKey(1)
-        if key in [27, 32, ord('q')]: # Salir del programa si alguna de estas teclas son presionadas {ESC, SPACE, q}
-            break
-        elif key == ord('w'):
-            y -= step
-        elif key == ord('a'):
-            x -= step
-        elif key == ord('s'):
-            y += step
-        elif key == ord('d'):
-            x += step
-        elif key == ord('r'): # Increase Delta
-            if delta < 50:
-                delta += 1
-                hostSpatials.setDeltaRoi(delta)
-        elif key == ord('f'): # Decrease Delta
-            if 3 < delta:
-                delta -= 1
-                hostSpatials.setDeltaRoi(delta)
+    text.rectangle(disp, (x-delta, y-delta), (x+delta, y+delta))
+    text.putText(disp, "X: " + ("{:.1f}m".format(spatials['x']/1000) if not math.isnan(spatials['x']) else "--"), (x + 10, y + 20))
+    text.putText(disp, "Y: " + ("{:.1f}m".format(spatials['y']/1000) if not math.isnan(spatials['y']) else "--"), (x + 10, y + 35))
+    text.putText(disp, "Z: " + ("{:.1f}m".format(spatials['z']/1000) if not math.isnan(spatials['z']) else "--"), (x + 10, y + 50))
+
+    # Show the frame
+    cv2.imshow("depth", disp)
+
+    key = cv2.waitKey(1)
+    if key in [27, 32, ord('q')]: # Salir del programa si alguna de estas teclas son presionadas {ESC, SPACE, q}
+        break
+    elif key == ord('w'):
+        y -= step
+    elif key == ord('a'):
+        x -= step
+    elif key == ord('s'):
+        y += step
+    elif key == ord('d'):
+        x += step
+    elif key == ord('r'): # Increase Delta
+        if delta < 50:
+            delta += 1
+            hostSpatials.setDeltaRoi(delta)
+    elif key == ord('f'): # Decrease Delta
+        if 3 < delta:
+            delta -= 1
+            hostSpatials.setDeltaRoi(delta)
