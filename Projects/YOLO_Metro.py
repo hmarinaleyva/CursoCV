ee9474f0542fc096bbb98054437dac93cd9951d6
CimttGit <cursos.cimtt@gmail.com>
2022-09-16 12:25:37 -0300

    borrados:        Projects/ArduinoSerial.py modificados:     Projects/YOLO_Metro.py nuevos archivos: Projects/all copy.py modificados:     Projects/all.py

--- Projects/YOLO_Metro.py
+++ Projects/YOLO_Metro.py
@@ -1,5 +1,5 @@
-from utilities import *
-from depthai_sdk import Previews, FPSHandler
+from depthai_sdk import PipelineManager, NNetManager, PreviewManager, Previews, FPSHandler, toTensorResult
+from depthai_sdk import Previews, FPSHandler, utils
 from depthai_sdk.managers import PipelineManager, PreviewManager, BlobManager, NNetManager
 import depthai as dai
 import cv2, os
@@ -15,40 +15,6 @@ CONFIG_PATH = os.path.join(MainDir, '../Models/MetroModel_YOLOv5s', "Metro.json"
 MODEL_PATH  = os.path.join(MainDir, '../Models/yolov5s/', "yolov5s_openvino_2021.4_6shave.blob")
 CONFIG_PATH = os.path.join(MainDir, '../Models/yolov5s/', "yolov5s.json")
 
-#######################################################################################################################
-
-# Create pipeline
-pipeline = dai.Pipeline()
-
-# Define sources and outputs
-monoLeft = pipeline.create(dai.node.MonoCamera)
-monoRight = pipeline.create(dai.node.MonoCamera)
-stereo = pipeline.create(dai.node.StereoDepth)
-
-# Properties
-monoLeft.setResolution(dai.MonoCameraProperties.SensorResolution.THE_400_P)
-monoLeft.setBoardSocket(dai.CameraBoardSocket.LEFT)
-monoRight.setResolution(dai.MonoCameraProperties.SensorResolution.THE_400_P)
-monoRight.setBoardSocket(dai.CameraBoardSocket.RIGHT)
-
-stereo.initialConfig.setConfidenceThreshold(255)
-stereo.setLeftRightCheck(True)
-stereo.setSubpixel(False)
-
-# Linking
-monoLeft.out.link(stereo.left)
-monoRight.out.link(stereo.right)
-
-xoutDepth = pipeline.create(dai.node.XLinkOut)
-xoutDepth.setStreamName("depth")
-stereo.depth.link(xoutDepth.input)
-
-xoutDepth = pipeline.create(dai.node.XLinkOut)
-xoutDepth.setStreamName("disp")
-stereo.disparity.link(xoutDepth.input)
-
-#######################################################################################################################
-
 
 # initialize blob manager with path to the blob
 bm = BlobManager(blobPath=MODEL_PATH)
@@ -63,94 +29,43 @@ pm.createColorCam(previewSize=nm.inputSize, xout=True)
 fpsHandler = FPSHandler()
 pv = PreviewManager(display=[Previews.color.name], fpsHandler=fpsHandler)
 
-# create depthai.node.NeuralNetwork
-YoloNN = nm.createNN(pipeline=pm.pipeline, nodes=pm.nodes, source=Previews.color.name,
+# create NN with managers
+nn = nm.createNN(pipeline=pm.pipeline, nodes=pm.nodes, source=Previews.color.name,
                  blobPath=bm.getBlob(shaves=6, openvinoVersion=pm.pipeline.getOpenVINOVersion(), zooType="depthai"))
+pm.addNn(nn)
 
-#pm.addNn(YoloNN)
-pm.addNn(pipeline)
+# initialize pipeline
+with dai.Device(pm.pipeline) as device:
+    # create outputs
+    pv.createQueues(device)
+    nm.createQueues(device)
+    xoutBoundingBoxDepthMappingQueue = device.getOutputQueue(name="boundingBoxDepthMapping", maxSize=4, blocking=False)
 
-#######################################################################################################################
-## initialize pipeline
-#with dai.Device(pipeline) as device:
-#    # create outputs
-#    pv.createQueues(device)
-#    nm.createQueues(device)
-#
-#    nnData = []
-#
-#    while True:
-#
-#        # parse outputs
-#        pv.prepareFrames()
-#        inNn = nm.outputQueue.tryGet()
-#
-#        if inNn is not None:
-#            nnData = nm.decode(inNn)
-#            # count FPS
-#            fpsHandler.tick("color")
-#
-#        nm.draw(pv, nnData)
-#        pv.showFrames()
-#
-#        # Salir del programa si alguna de estas teclas son presionadas {ESC, SPACE, q} 
-#        if cv2.waitKey(1) in [27, 32, ord('q')]:
-#            break
+    nnData = []
 
+    while True:
 
-##########################################################################################################
+        #parse outputs
+        pv.prepareFrames()
+        inNn = nm.outputQueue.tryGet()
+        inDet = nm.outputQueue.get()
 
+        detections = inDet.detections
+        if len(detections) != 0:
+            boundingBoxMapping = xoutBoundingBoxDepthMappingQueue.get()
+            roiDatas = boundingBoxMapping.getConfigData()
+            print(roiDatas)
 
-# Connect to device and start pipeline
-device = dai.Device(pipeline)
+        #if inNn is not None:
+        #    nnData = nm.decode(inNn)
+        #    fpsHandler.tick("color")
+        #    detections = [ type(Det) for Det in nnData]
+        #    #print(pv.get('host'))
 
-# Output queue will be used to get the depth frames from the outputs defined above
-depthQueue = device.getOutputQueue(name="depth")
-dispQ = device.getOutputQueue(name="disp")
+        frame = pv.get(Previews.color.name)
+        nm.draw(pv, nnData)
+        pv.showFrames()
 
-text = TextHelper()
-hostSpatials = HostSpatialsCalc(device)
-y = 200
-x = 300
-step = 3
-delta = 5
-hostSpatials.setDeltaRoi(delta)
-
-print("Use WASD keys to move ROI.\nUse 'r' and 'f' to change ROI size.")
-
-while True:
-    depthFrame = depthQueue.get().getFrame()
-    # Calculate spatial coordiantes from depth frame
-    spatials, centroid = hostSpatials.calc_spatials(depthFrame, (x,y)) # centroid == x/y in our case
-
-    # Get disparity frame for nicer depth visualization
-    disp = dispQ.get().getFrame()
-    disp = cv2.applyColorMap(disp, cv2.COLORMAP_JET)
-
-    text.rectangle(disp, (x-delta, y-delta), (x+delta, y+delta))
-    text.putText(disp, "X: " + ("{:.2f}m".format(spatials['x']/1000) if not math.isnan(spatials['x']) else "--"), (x + 10, y + 20))
-    text.putText(disp, "Y: " + ("{:.2f}m".format(spatials['y']/1000) if not math.isnan(spatials['y']) else "--"), (x + 10, y + 35))
-    text.putText(disp, "Z: " + ("{:.2f}m".format(spatials['z']/1000) if not math.isnan(spatials['z']) else "--"), (x + 10, y + 50))
-
-    # Show the frame
-    cv2.imshow("depth", disp)
-
-    key = cv2.waitKey(1)
-    if key in [27, 32, ord('q')]: # Salir del programa si alguna de estas teclas son presionadas {ESC, SPACE, q}
-        break
-    elif key == ord('w'):
-        y -= step
-    elif key == ord('a'):
-        x -= step
-    elif key == ord('s'):
-        y += step
-    elif key == ord('d'):
-        x += step
-    elif key == ord('r'): # Increase Delta
-        if delta < 50:
-            delta += 1
-            hostSpatials.setDeltaRoi(delta)
-    elif key == ord('f'): # Decrease Delta
-        if 3 < delta:
-            delta -= 1
-            hostSpatials.setDeltaRoi(delta)
+        # Salir del programa si alguna de estas teclas son presionadas {ESC, SPACE, q} 
+        if cv2.waitKey(1) in [27, 32, ord('q')]:
+            break
