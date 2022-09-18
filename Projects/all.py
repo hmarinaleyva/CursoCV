615267f9f9a64f7aebf97fcf5ad444da5e91f3bf
CimttGit <cursos.cimtt@gmail.com>
2022-09-18 15:49:09 -0300

    132

--- Projects/all.py
+++ Projects/all.py
@@ -1,9 +1,9 @@
 import serial, subprocess
 
 try: #intenta abrir el puerto serie
-    InfoBoard = subprocess.getoutput('arduino-cli board list').split()
-    PuertoArduino  = InfoBoard[9] #Obtener el puerto de la placa Arduino
-    FQBN  = InfoBoard[16] #Obtener el FQBN de la placa Arduino
+    InfoBoard = subprocess.getoutput('arduino-cli board list').split("\n")[1].split() 
+    PuertoArduino  = InfoBoard[0] #Obtener el puerto de la placa Arduino
+    FQBN  = InfoBoard[-2] #Obtener el FQBN de la placa Arduino
     ArduinoSerial = serial.Serial(PuertoArduino, 9600, timeout=1) #abrir el puerto serie
 except:
     print("No se estableció comunicación serial con una placa Arduino correctamente")
